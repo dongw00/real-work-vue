b7dd9112439cd32d824a35c63862f9c8a87bb255
dongw00 <albadw0428@gmail.com>
2021-02-27 23:59:44 +0900

    real word vue 3 update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
   </div>
   <router-view />
 </template>
@@ -26,5 +28,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
